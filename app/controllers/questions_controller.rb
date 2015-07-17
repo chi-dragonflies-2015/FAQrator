59e1369037968ffd0028199b947cb1c8e5a01040
Henry Firth <henrynf12@gmail.com>
2015-07-17 16:04:18 -0500

    Add question creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,11 +6,18 @@ class QuestionsController < ApplicationController
   end
 
   def create
-
+    @question = Question.create(question_params)
+    redirect_to "/questions"
   end
 
   def destroy
 
+  end
+
+private
+
+  def question_params
+    params.require(:question).permit(:content, :answer)
   end
 
 end
