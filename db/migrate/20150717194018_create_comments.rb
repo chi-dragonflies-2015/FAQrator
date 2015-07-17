03eeabb9adc6ec1e67795fc3d53baa17423f4206
Faithsend <matt@sandfalls.com>
2015-07-17 15:04:39 -0500

    comment migration

--- db/migrate/20150717194018_create_comments.rb
+++ db/migrate/20150717194018_create_comments.rb
@@ -2,7 +2,9 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.references :question
+      t.text :content, null: false
 
+      t.timestamps
     end
   end
 end
