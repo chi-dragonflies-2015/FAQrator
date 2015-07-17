52007ab108df35219fef7e68c129a9b3e94d96d2
ac-adekunle <lola.adekunle@gmail.com>
2015-07-17 18:32:19 -0500

    Add topics#show to topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,9 @@ class TopicsController < ApplicationController
   def index
   	@topics = Topic.all
   end
+
+  def show
+  	@topic = Topic.find(params[:id])
+  	@questions = @topic.questions
+  end
 end
