32695a2115b5b22ff473016b58de2fb91dfd5a8a
ac-adekunle <lola.adekunle@gmail.com>
2015-07-18 11:29:20 -0500

    Add edit, update,and  delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
 
-  resources :topics
+  resources :topics, :only => [:index, :show, :new, :create]
+
+
+  get '/topics/:slug/:secret_url' => 'topics#edit', as: 'edit_topic'
+
+  put '/topics/:slug' => 'topics#update'
+
+  delete '/topics/:slug' => 'topics#destroy', as: 'delete_topic'
   
 end
