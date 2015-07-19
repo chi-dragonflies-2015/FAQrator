3af2d3026f3ceba49cddcbdbf6d907b907224292
Faithsend <matt@sandfalls.com>
2015-07-19 17:44:04 -0500

    fix migration issue for reals

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150718203802) do
+ActiveRecord::Schema.define(version: 20150718234527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "question_id"
     t.text     "content",     null: false
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20150718203802) do
     t.string   "author"
     t.text     "response"
   end
-end
-
-ActiveRecord::Schema.define(version: 20150718234527) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
     t.string   "content"
