b49e48c4eb364c24ff487f6f5eb46d3d4c13e982
Henry Firth <henrynf12@gmail.com>
2015-07-20 16:38:02 -0500

    Add #current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def render_404
     render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 end
