25a6cf5e9504c78ceee5f93cb828a89a8ff6738d
Henry Firth <henrynf12@gmail.com>
2015-07-20 15:38:07 -0500

    Add new sessions route & action

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   resources :topics, :only => [:index, :show, :new, :create, :update, :destroy]
   get '/topics/:id/:edit_key' => 'topics#edit', as: 'edit_topic'
   post '/topics/search' => 'topics#search'
-  get '/sessions/new' => 'users#login'
-  delete '/sessions' => 'users#logout'
+  get '/sessions/new' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
   resources :users, :except => :index
   resources :questions
   resources :comments, :only => [:create, :update, :destroy]
