060acfa0e1337d9223f3e36b9a594849552424e2
Henry Firth <henrynf12@gmail.com>
2015-07-20 15:12:28 -0500

    Add create action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
 
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to 'new'
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
   end
 
   def login
@@ -11,4 +24,11 @@ class UsersController < ApplicationController
   def logout
 
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
+
 end
