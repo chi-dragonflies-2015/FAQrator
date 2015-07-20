1c3c2ca945a1cb6f8d413275f39c61c831bb8322
ac-adekunle <lola.adekunle@gmail.com>
2015-07-20 10:19:20 -0500

    Update topics feature specs to include search form

--- spec/features/topics_feature_spec.rb
+++ spec/features/topics_feature_spec.rb
@@ -6,7 +6,7 @@ feature "User visits the website" do
     visit '/'
     fill_in('topic[title]', :with => topic.title)
     fill_in('topic[description]', :with => topic.description)
-    click_button 'submit'
+    click_button 'new_submit'
     expect(page).to have_content(topic.title)
     expect(page).to have_content topic.description
   end
@@ -31,8 +31,20 @@ feature "User posts a new topic" do
     visit 'topics/new'
     fill_in('topic[title]', :with => topic.title)
     fill_in('topic[description]', :with => topic.description)
-    click_button 'submit'
+    click_button 'new_submit'
     expect(page).to have_content topic.title
     expect(page).to have_content topic.description
+  end
+end
+
+feature "User can search for a topic" do
+  scenario "when user searches for a topic title" do
+    Topic.create(title: "Romans", description: "Old School" )
+    topic = Topic.create(title: "Hangry", description: "Hungry and Angry" )
+    visit '/topics'
+    fill_in('search', :with => 'hangry')
+    click_button 'search_submit'
+    expect(page).to have_content topic.title
+    expect(page).to_not have_content("Romans")
   end
 end
