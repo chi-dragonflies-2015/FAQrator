7f4a6358391f2e9092e81d5da7e0068096c25068
Faithsend <matt@sandfalls.com>
2015-07-20 10:10:01 -0500

    add presence validation to question comment

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ActiveRecord::Base
   has_many :comments
   belongs_to :topic
+
+  validates :content, presence: true
+  validates :topic, presence: true
 end
+
+#six and 140 character limit
