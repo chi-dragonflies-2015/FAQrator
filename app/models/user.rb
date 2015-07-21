c86dbfa602aa9dbdc74fad56669b9ae13bf99b30
ac-adekunle <lola.adekunle@gmail.com>
2015-07-21 10:46:30 -0500

    Add validations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_many :topics
 
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
