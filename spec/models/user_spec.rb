6fef194079c47d558ffa102f219bff1a9fcc03aa
ac-adekunle <lola.adekunle@gmail.com>
2015-07-21 18:13:27 -0500

    Refactor user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,20 @@ describe User do
 
   describe 'Following' do
     before(:each) do
-      @user1 = build(:user)
-      @user2 = build(:user)
-      
+      @user1 = create(:user)
+      @user2 = create(:user)
+      @user1.follow(@user2)
     end
 
     it "follows other users correctly" do
-      @user1.follow(@user2)
-      expect(@user1.followers).to include @user2
+      expect(@user1.following).to include @user2
     end
 
     it "unfollows other users correctly" do
       @user1.unfollow(@user2)
-      expect(@user1.followers).not_to include @user2
+      expect(@user1.following).not_to include @user2
     end
 
-
   end
+  
 end
