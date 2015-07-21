02aea878ba3d53cc8d7443065f017845a175a98e
ac-adekunle <lola.adekunle@gmail.com>
2015-07-21 18:16:55 -0500

    Update ar associations in user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :topics
+  
   has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id", dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :first_name, presence: true
   validates :last_name, presence: true
