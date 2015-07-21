2455214859ff6e320dab634cea11a5f0a16c0dcc
ac-adekunle <lola.adekunle@gmail.com>
2015-07-21 18:41:22 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,31 @@
-5.times do
-	FactoryGirl.create(:topic)
+
+# Users
+20.times do |n|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  username  = Faker::Internet.user_name
+  email = "example-#{n+1}@example.org"
+  password = "password"
+  User.create!( first_name: first_name,
+  				last_name: last_name,
+  				username: username,
+              	email: email,
+              	password: password,
+         )
 end
+
+# Topics
+users = User.all
+rand(5..10).times do
+  title = Faker::Hacker.say_something_smart
+  description = Faker::Lorem.paragraph
+  users.each { |user| user.topics.create!(title: title, description: description ) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..20]
+followers = users[3..10]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
