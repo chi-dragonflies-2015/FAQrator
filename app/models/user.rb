876c78167384576faefcca1f618fd9ab534b79a8
ac-adekunle <lola.adekunle@gmail.com>
2015-07-21 18:14:07 -0500

    Add follow and unfollow methods to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :topics
-  has_many :active_relationshhips, class_name:  "Relationship",foreign_key: "follower_id", dependent:   :destroy
+  has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id", dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
 
   validates :first_name, presence: true
@@ -10,4 +10,21 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
+
+
+ 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
