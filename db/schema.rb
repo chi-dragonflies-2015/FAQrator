0a97dd0af159fe163c6d307d61e2fb97fef7c86b
Henry Firth <henrynf12@gmail.com>
2015-07-22 15:19:17 -0500

    Add primary_color column to topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720191305) do
+ActiveRecord::Schema.define(version: 20150722201516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,25 @@ ActiveRecord::Schema.define(version: 20150720191305) do
   create_table "questions", force: :cascade do |t|
     t.string   "content"
     t.text     "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "topic_id"
+    t.integer  "cached_votes_total",      default: 0
+    t.integer  "cached_votes_score",      default: 0
+    t.integer  "cached_votes_up",         default: 0
+    t.integer  "cached_votes_down",       default: 0
+    t.integer  "cached_weighted_score",   default: 0
+    t.integer  "cached_weighted_total",   default: 0
+    t.float    "cached_weighted_average", default: 0.0
   end
+
+  add_index "questions", ["cached_votes_down"], name: "index_questions_on_cached_votes_down", using: :btree
+  add_index "questions", ["cached_votes_score"], name: "index_questions_on_cached_votes_score", using: :btree
+  add_index "questions", ["cached_votes_total"], name: "index_questions_on_cached_votes_total", using: :btree
+  add_index "questions", ["cached_votes_up"], name: "index_questions_on_cached_votes_up", using: :btree
+  add_index "questions", ["cached_weighted_average"], name: "index_questions_on_cached_weighted_average", using: :btree
+  add_index "questions", ["cached_weighted_score"], name: "index_questions_on_cached_weighted_score", using: :btree
+  add_index "questions", ["cached_weighted_total"], name: "index_questions_on_cached_weighted_total", using: :btree
 
   create_table "topics", force: :cascade do |t|
     t.string  "title"
@@ -39,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150720191305) do
     t.string  "edit_key"
     t.string  "slug"
     t.integer "user_id"
+    t.string  "primary_color"
   end
 
   add_index "topics", ["slug"], name: "index_topics_on_slug", using: :btree
@@ -53,6 +69,21 @@ ActiveRecord::Schema.define(version: 20150720191305) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
   add_foreign_key "topics", "users"
 end
