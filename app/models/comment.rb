46eb9d843d15a1cd1b4dd09d3653d29265346120
Faithsend <matt@sandfalls.com>
2015-07-23 10:47:34 -0500

    add nil check to authors for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,6 @@ class Comment < ActiveRecord::Base
   before_save :init
 
   def init
-    self.author = "Guest#{rand(1000)}" if author == ""
+    self.author = "Guest#{rand(1000)}" if author == "" || author == nil
   end
 end
